3231a4af148bf0182b68608781e816b8c83536d5
FaNim21 <filipekbwo@wp.pl>
2022-06-15 17:31:26 +0200

    Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='NecesseFishingBot',
+    version='1.0.1',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Filip "FaNim" Gorczyca',
+    author_email='',
+    description='Bot for fishing in Necesse'
+)
